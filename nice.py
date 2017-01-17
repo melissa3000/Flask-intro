386083f7b28b25f73449cc5e1c671ddee6499a58
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-17 13:03:51 -0800

    Flask intro

---
+++ nice.py
@@ -0,0 +1,77 @@
+from random import choice
+
+from flask import Flask, request
+
+
+# "__name__" is a special Python variable for the name of the current module
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza', 'oh-so-not-meh',
+    'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful', 'smashing', 'lovely']
+
+
+@app.route('/')
+def start_here():
+    """Home page."""
+
+    return """
+    <!doctype html>
+      <html>
+        <head>Hi! This is the home page.</head>
+        <body><a href="/hello">hello</a></body>
+      <html>"""
+
+
+@app.route('/hello')
+def say_hello():
+    """Say hello and prompt for user's name."""
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>Hi There!</title>
+      </head>
+      <body>
+        <h1>Hi There!</h1>
+        <form action="/greet">
+          <label>What's your name? <input type="text" name="person"></label><br>
+          <input type="radio" name="compliment" value="awesome">awesome
+          <input type="radio" name="compliment" value="terrific">terrific
+          <input type="radio" name="compliment" value="fantastic">fantastic<br>
+          <input type="submit">
+        </form>
+      </body>
+    </html>
+    """
+
+
+@app.route('/greet')
+def greet_person():
+    """Get user by name."""
+
+    player = request.args.get("person")
+
+    # compliment = choice(AWESOMENESS)
+    compliment = request.args.get("compliment")
+
+    print x
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Compliment</title>
+      </head>
+      <body>
+        Hi %s I think you're %s!
+      </body>
+    </html>
+    """ % (player, compliment)
+
+
+if __name__ == '__main__':
+    # debug=True gives us error messages in the browser and also "reloads"
+    # our web app if we change the code.
+    app.run(debug=True)
